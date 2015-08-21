fedc376837920a941c05cb4fb752f30921e489dc
Richard Yen <richyen3@gmail.com>
2015-08-20 23:00:07 -0700

    Include env variables

--- pg/xdb_add_test/setup_xdb_containers.sh
+++ pg/xdb_add_test/setup_xdb_containers.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
+
+. /Users/richyen/Code/pg/edb/ppas_and_docker/docker_functions
 for i in 1 2 3 4 5 6; do docker rm -f xdb$i & done; docker rm -f xdb
 for i in 1 2 3 4 5 6; do create_container xdb$i xdb5:6.6 & done; create_container xdb xdb5:6.6
+sleep 10
 docker ps -a | grep xdb | awk '{ print $17 }' | xargs -I% docker inspect % | grep -i ipadd | grep -v Second | cut -f4 -d'"'
 
