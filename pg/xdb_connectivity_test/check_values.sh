8a897ddaf90be57775195eb587caa9c377b316a4
Richard Yen <richyen3@gmail.com>
2016-06-10 10:37:51 -0700

    Forgot a couple files with last checkin

--- pg/xdb_connectivity_test/check_values.sh
+++ pg/xdb_connectivity_test/check_values.sh
@@ -1,30 +1,34 @@
 #!/bin/bash
 
 first=1
+num_fails=0
+
+TABLE="erep_pub_database"
+DB='xdb_ctl'
 
 if [[ ${1} -eq 6 ]]
 then
-  TABLE="_edb_replicator_pub.xdb_pub_database"
+  TABLE="xdb_pub_database"
   DB=''
-else
-  TABLE="_edb_replicator_pub.erep_pub_database"
-  DB='xdb_ctl'
 fi
 
-for i in `psql -Atc "select db_host from ${TABLE}" ${DB}`
+for i in `psql -Atc "select db_host from _edb_replicator_pub.${TABLE}" ${DB}`
 do
-
- VAL=`psql -Atc "select rtrim(filler,' ') from pgbench_accounts where aid=100" -h $i`
-
- if [[ $first -gt 0 ]]
- then
-   correct=${VAL}
-   printf "\e[0;33m MDN (host $i) value is '$correct'\n \e[0m"
- elif [[ $correct == $VAL ]]
- then
-   printf "\e[0;32m Replicated $correct == $VAL on host $i \n \e[0m"
- else
-   printf "\e[0;31m NOT REPLICATED $correct != $VAL on host $i \n \e[0m"
- fi
- first=0
+  VAL=`psql -Atc "select rtrim(filler,' ') from pgbench_accounts where aid=100" -h $i`
+ 
+  if [[ $first -gt 0 ]]
+  then
+    correct=${VAL}
+    # printf "\e[0;33m MDN (host $i) value is '$correct'\n \e[0m"
+  elif [[ ${correct} == ${VAL} ]]
+  then
+    # printf "\e[0;32m Replicated $correct == $VAL on host $i \n \e[0m"
+    first=0
+  else
+    # printf "\e[0;31m NOT REPLICATED $correct != $VAL on host $i \n \e[0m"
+    ((num_fails++))
+  fi
+  first=0
 done
+
+echo "Failed ${num_fails} times"
--- pg/xdb_connectivity_test/connectivity_demo.sh
+++ pg/xdb_connectivity_test/connectivity_demo.sh
@@ -1,26 +1,57 @@
 #!/bin/bash
 
-P="/Desktop"
-IP=`docker inspect xdb5-1 | grep '"IPAddress"' | awk '{ print $2 }' | cut -f 2 -d '"' | sort -u | head -n 1`
+P="/Desktop/toolbox/pg/xdb_connectivity_test"
 
+XDB_VER=${1}
+if [[ "x${XDB_VER}" == "x" ]]
+then
+  XDB_VER=5
+fi
+
+if [[ ${XDB_VER} -eq 6 ]]
+then
+  XDB_MAJOR=6.0
+else
+  XDB_MAJOR=5.1
+fi
+
+IP=`docker inspect xdb${XDB_VER}-5 | grep '"IPAddress"' | awk '{ print $2 }' | cut -f 2 -d '"' | sort -u | head -n 1`
 set -x
-# docker exec xdb1 java -jar /usr/ppas-xdb-5.1/bin/edb-repcli.jar -version # for XDB 5.1
-docker exec xdb1 java -jar /usr/ppas-xdb-6.0/bin/edb-repcli.jar -version # for XDB 6.0
-docker exec xdb1 ${P}/show_servers.sh
-docker exec xdb1 ${P}/check_values.sh
-docker exec xdb1 ${P}/do_update.sh jeremy
+
+### SHOW XDB VERSION & SETUP ###
+docker exec xdb${XDB_VER}-1 java -jar /usr/ppas-xdb-${XDB_MAJOR}/bin/edb-repcli.jar -version
+docker exec xdb${XDB_VER}-1 ${P}/show_servers.sh ${XDB_VER}
+docker exec xdb${XDB_VER}-1 ${P}/do_update.sh first
 sleep 10 # Let XDB have some time to sync
-docker exec xdb1 ${P}/check_values.sh
-docker exec xdb4 ${P}/toggle_ip.sh del ${IP} # SHUTTING OFF eth0
-docker exec xdb1 ${P}/check_values.sh
-docker exec xdb1 ${P}/do_update.sh thomas
+
+### VERIFY REPLICATION IS WORKING ###
+docker exec xdb${XDB_VER}-1 ${P}/check_values.sh ${XDB_VER}
+docker exec xdb${XDB_VER}-1 ${P}/do_update.sh second
 sleep 10 # Let XDB have some time to sync
-docker exec xdb1 ${P}/check_values.sh
+docker exec xdb${XDB_VER}-1 ${P}/check_values.sh ${XDB_VER}
+
+### SIMULATE NETWORK OUTAGE ###
+docker exec xdb${XDB_VER}-5 ${P}/toggle_ip.sh del ${IP}                 # SHUTTING OFF eth0
+
+### VERIFY NETWORK DISCONNECTED ###
+docker exec xdb${XDB_VER}-1 ${P}/check_values.sh ${XDB_VER}
+docker exec xdb${XDB_VER}-1 ${P}/show_xdb_log.sh
+
+### DEMONSTRATE REPLICATION HALTED FOR ALL NODES ###
+docker exec xdb${XDB_VER}-1 ${P}/do_update.sh third
+sleep 10 # Let XDB have some time to sync
+docker exec xdb${XDB_VER}-1 ${P}/check_values.sh ${XDB_VER}
+docker exec xdb${XDB_VER}-1 ${P}/show_xdb_log.sh
 sleep 30
-docker exec xdb1 ${P}/check_values.sh # Try again
-docker exec xdb4 ${P}/toggle_ip.sh add ${IP} # RESTART eth0
-sleep 30
-docker exec xdb1 ${P}/check_values.sh
-docker exec xdb1 ${P}/do_update.sh eric
-sleep 30 # Let XDB have some time to sync
-docker exec xdb1 ${P}/check_values.sh
+docker exec xdb${XDB_VER}-1 ${P}/check_values.sh ${XDB_VER}             # Try again
+
+### RECONNECT NETWORK ###
+docker exec xdb${XDB_VER}-5 ${P}/toggle_ip.sh add ${IP}                 # RESTART eth0
+
+### VERIFY REPLICATION RECOVERED ###
+sleep 45 # Give it some time to self-heal
+docker exec xdb${XDB_VER}-1 ${P}/check_values.sh ${XDB_VER}
+docker exec xdb${XDB_VER}-1 ${P}/do_update.sh fourth
+sleep 45 # Let XDB have some time to sync
+docker exec xdb${XDB_VER}-1 ${P}/check_values.sh ${XDB_VER}
+# docker exec xdb${XDB_VER}-1 ${P}/show_rep_history.sh ${XDB_VER}       # show rep events history for past one day
