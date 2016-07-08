238531bd4146359d3f2d4eb505fe3b265aeea4c9
Richard Yen <richyen3@gmail.com>
2016-07-08 16:37:17 -0700

    Better comments and timings Add sleep while we wait for queries to get canceled

--- pg/streaming_replication_tests/max_standby_streaming_delay_test.sh
+++ pg/streaming_replication_tests/max_standby_streaming_delay_test.sh
@@ -6,16 +6,18 @@
 MASTER_IP=172.17.0.6
 
 # Setup
+dropdb -h ${MASTER_IP} bench
 createdb -h ${MASTER_IP} bench
 pgbench -i -F 50 -s 50 -h ${MASTER_IP} bench 2> /dev/null
+
+# See if max_standby_streaming_delay cancels a query
 sed -i "s/max_standby_streaming_delay.*/max_standby_streaming_delay = 10s/" /var/lib/ppas/9.5/data/postgresql.conf 
 psql -Atc "select pg_reload_conf()"
-
-
+psql -Atc "show max_standby_streaming_delay"
 psql -h ${MASTER_IP} -c "DELETE FROM pgbench_accounts where aid between 0 and 10000" bench
-psql -c "select pg_sleep(30), count(*) from pgbench_accounts" bench &
+psql -c "select pg_sleep(45), count(*) from pgbench_accounts" bench &
 psql -h ${MASTER_IP} -c "vacuum" bench
-# Wait for pg_sleep to get killed
+sleep 120 # Wait for pg_sleep to get canceled
 
 # Try again
 sed -i "s/max_standby_streaming_delay.*/max_standby_streaming_delay = 60s/" /var/lib/ppas/9.5/data/postgresql.conf 
@@ -24,12 +26,13 @@ psql -Atc "show max_standby_streaming_delay"
 psql -h ${MASTER_IP} -c "DELETE FROM pgbench_accounts where aid between 10000 and 20000" bench
 psql -c "select pg_sleep(30), count(*) from pgbench_accounts" bench &
 psql -h ${MASTER_IP} -c "vacuum" bench
-# This pg_sleep should succeed
+sleep 120 # This pg_sleep should succeed
 
+# Try with longer sleep
 psql -h ${MASTER_IP} -c "DELETE FROM pgbench_accounts where aid between 20000 and 30000" bench
 psql -c "select pg_sleep(120), count(*) from pgbench_accounts" bench &
 psql -h ${MASTER_IP} -c "vacuum" bench
-# This pg_sleep should fail
+sleep 120 # This pg_sleep should get canceled
 
 # Try again
 sed -i "s/max_standby_streaming_delay.*/max_standby_streaming_delay = 120s/" /var/lib/ppas/9.5/data/postgresql.conf 
@@ -38,7 +41,7 @@ psql -Atc "show max_standby_streaming_delay"
 psql -h ${MASTER_IP} -c "DELETE FROM pgbench_accounts where aid between 30000 and 40000" bench
 psql -c "select pg_sleep(60), count(*) from pgbench_accounts" bench &
 psql -h ${MASTER_IP} -c "vacuum" bench
-# This pg_sleep should succeed
+sleep 120 # This pg_sleep should succeed
 
 # Check the number of conflicts recorded
 psql -c "select * from pg_stat_database_conflicts where datname = 'bench'"
