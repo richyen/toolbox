70dc08a19dd1583b4803c6e0a1e6d736a653c9fd
Richard Yen <richyen@users.noreply.github.com>
2017-11-16 16:36:29 -0800

    Better interpolation support

--- pg/edb/epas/vm_setup.sh
+++ pg/edb/epas/vm_setup.sh
@@ -12,7 +12,7 @@ PATH=/usr/ppas-${PGMAJOR}/bin:${PATH}
 PGDATA=/var/lib/ppas/${PGMAJOR}/data
 PGLOG=/var/lib/ppas/${PGMAJOR}/pgstartup.log
 XDB_VERSION=6.0
-INSTALLDIR=/usr/ppas-xdb-${XDB_VERSION}
+XDB_INSTALLDIR=/usr/ppas-xdb-${XDB_VERSION}
 JAVA_VERSION=1.7
 EDBUSERNAME="richard.yen@enterprisedb.com"
 YUMUSERNAME=edb-richardyen                               
@@ -30,7 +30,7 @@ echo 'root:root'|chpasswd
 adduser --home-dir /home/postgres --create-home postgres
 echo 'postgres   ALL=(ALL)   NOPASSWD: ALL' >> /etc/sudoers
 echo 'postgres:postgres'|chpasswd
-rm -rf /var/lib/ppas/9.5/data/
+rm -rf ${PGDATA{
 sudo -u enterprisedb /usr/ppas-9.5/bin/initdb -D ${PGDATA}  
 sed -i "s/^PGPORT.*/PGPORT=${PGPORT}/" /etc/sysconfig/ppas/ppas-${PGMAJOR}
 echo "export PGPORT=${PGPORT}"         >> /etc/profile.d/pg_env.sh
@@ -43,22 +43,23 @@ echo "host   replication enterprisedb  0.0.0.0/0  trust" >> ${PGDATA}/pg_hba.con
 echo "host   all         all      0.0.0.0/0  trust" >> ${PGDATA}/pg_hba.conf
 
 ### For streaming replication, if desired
-# sudo -u enterprisedb /usr/ppas-9.5/bin/pg_basebackup -xRP -h 10.228.145.74 -p5432 -D /var/lib/ppas/9.5/data
+# sudo -u enterprisedb /usr/ppas-9.5/bin/pg_basebackup -xRP -h 10.228.145.74 -p5432 -D ${PGDATA}
 
 ### TODO: Put your desired postgresql.conf in here if you want something customized
-# cat /home/ec2-user/postgresql.conf > ${PGDATA}/postgresql.conf
+# cat ${HOME}/postgresql.conf > ${PGDATA}/postgresql.conf
 service ppas-9.5 start
 
 ### For XDB, if needed
 # wget http://get.enterprisedb.com/xdb/xdbreplicationserver-6.1.2-1-linux-x64.run
 # chmod 755 xdbreplicationserver-6.1.2-1-linux-x64.run 
-# ./xdbreplicationserver-6.1.2-1-linux-x64.run --existing-user ${EDBUSERNAME} --existing-password ${EDBPASSWORD} --mode unattended --admin_user enterprisedb --admin_password abc123 --prefix ${INSTALLDIR}
-# echo "user=enterprisedb" > /usr/ppas-xdb-6.0/etc/xdb_repsvrfile.conf
-# echo "password=Cz0Ccyegvs8=" >> /usr/ppas-xdb-6.0/etc/xdb_repsvrfile.conf
-# echo "port=9051" >> /usr/ppas-xdb-6.0/etc/xdb_repsvrfile.conf
-# echo "host=127.0.0.1" >> /usr/ppas-xdb-6.0/etc/xdb_repsvrfile.conf
+# ${HOME}/xdbreplicationserver-6.1.2-1-linux-x64.run --existing-user ${EDBUSERNAME} --existing-password ${EDBPASSWORD} --mode unattended --admin_user enterprisedb --admin_password abc123 --prefix ${INSTALLDIR}
+# echo "user=enterprisedb" > ${XDB_INSTALLDIR}/etc/xdb_repsvrfile.conf
+# echo "password=Cz0Ccyegvs8=" >> ${XDB_INSTALLDIR}/etc/xdb_repsvrfile.conf
+# echo "port=9051" >> ${XDB_INSTALLDIR}/etc/xdb_repsvrfile.conf
+# echo "host=127.0.0.1" >> ${XDB_INSTALLDIR}/etc/xdb_repsvrfile.conf
+# cat ${HOME}/xdb_pubserver.conf > ${XDB_INSTALLDIR}/etc/xdb_pubserver.conf
 
 ### TODO: Additional config required to make this work--sets up replication
-# /home/ec2-user//build_xdb_mmr_publication.sh 
+# ${HOME}/build_xdb_mmr_publication.sh 
 # psql -p 5432 edb enterprisedb < insert.sql 
-# /usr/ppas-9.5/bin/pgbench -P 30 -T 1800 -R 600 -f /home/ec2-user/bench.sql -p5432 -U enterprisedb edb 
+# /usr/ppas-9.5/bin/pgbench -P 30 -T 1800 -R 600 -f ${HOME}/bench.sql -p5432 -U enterprisedb edb 
