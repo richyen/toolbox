ea81c20471f68b2acbec3f8b06a1ed93c5129e9f
Richard Yen <richyen3@gmail.com>
2017-11-24 00:48:50 -0800

    Query update

--- vm/aws/edb/assessment/top_performers.sql
+++ vm/aws/edb/assessment/top_performers.sql
@@ -2,6 +2,7 @@ SELECT ename, sale_date, customer_name, sale_amount
   FROM sales s
   JOIN emp e ON e.empno=s.salesperson
   JOIN jobhist j ON j.empno=e.empno
- WHERE date_part('month',sale_date) = 11
+ WHERE date_part('month',sale_date) > 9
    AND j.startdate between '1980-01-01' and '1985-01-01'
+   AND sale_amount > 90
  ORDER BY sale_amount DESC;
