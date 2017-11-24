df041dacff5854f3c1fb1c7d14a27a4da898e992
Richard Yen <richyen3@gmail.com>
2017-11-24 00:45:25 -0800

    Better randomization of dates

--- vm/aws/edb/assessment/update_data.sh
+++ vm/aws/edb/assessment/update_data.sh
@@ -14,14 +14,13 @@ for empno in `psql -Atc "SELECT empno FROM emp"`
 done
 
 # Populate the sales table
-psql -c "INSERT INTO sales VALUES (generate_series(1001,2000),(SELECT max(empno) FROM emp),md5(random()), now(), (random()*100)::int);"
+psql -c "INSERT INTO sales VALUES (generate_series(1001,2000),(SELECT max(empno) FROM emp),md5(random()), now() - random() * interval '1 year', (random()*100)::int);"
 psql -c "UPDATE sales SET salesperson = e.empno, sale_amount = random() * 100 FROM (select distinct (empno % 13) + 1 as idx, * from emp) e WHERE (sales.id % 13) = e.idx"
 
-# Randomize sale dates and set the customer name for each row based on the empno/salesperson
+# Set the customer name for each row based on the empno/salesperson
 MAXID=`psql -Atc "SELECT max(id) FROM sales"`
 for id in `seq 1 ${MAXID}`
   do
-  psql -qc "UPDATE sales SET sale_date = (select current_date - random() * interval '1 year') WHERE id = ${id}"
   psql -qc "UPDATE sales SET customer_name = (SELECT 'Customer of ' || empno FROM emp WHERE empno = sales.salesperson)"
 done
 
