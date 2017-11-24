cf2989da9254cc358bb5a65875b35576bc0bf0f7
Richard Yen <richyen3@gmail.com>
2017-11-24 00:30:42 -0800

    Query updates

--- vm/aws/edb/assessment/top_performers.sql
+++ vm/aws/edb/assessment/top_performers.sql
@@ -2,7 +2,7 @@ SELECT ename, sale_date, customer_name, sale_amount
   FROM sales s
   JOIN emp e ON e.empno=s.salesperson
   JOIN jobhist j ON j.empno=e.empno
- WHERE sale_date > now() - interval '1 month'
+ WHERE date_part('month',sale_date) = 11
    AND j.startdate between '1980-01-01' and '1985-01-01'
  ORDER BY sale_amount
   DESC limit 10;
