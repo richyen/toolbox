30e05108f03b13b37c5146bb25944324488f2e35
Richard Yen <richyen3@gmail.com>
2018-02-08 17:14:37 -0800

    Set environment variables

--- pg/edb/epas/fdw_demo/fdw_demo.sh
+++ pg/edb/epas/fdw_demo/fdw_demo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Clean up
-docker rm -f oracle pgsql
+docker rm -f oracle
 
 # Create Oracle container
 docker run --privileged=true --publish-all=true --interactive=false --tty=true -v ${PWD}:/fdw_demo --hostname=oracle --detach=true --name=oracle wnameless/oracle-xe-11g:latest
@@ -16,7 +16,6 @@ docker exec -t oracle bash --login -c "sqlplus -S system/oracle < /fdw_demo/fdw_
 rm -f fdw_demo_data.sql
 
 # Create PG container
-docker rm -f pgsql
 docker run --privileged=true --publish-all=true --interactive=false --tty=true -v ${PWD}:/fdw_demo --hostname=pgsql  --detach=true --name=pgsql ppas95:latest
 docker exec -t pgsql yum -y install unzip git libaio
 docker exec -t pgsql yum -y groupinstall "Development Tools"
@@ -24,15 +23,20 @@ docker exec -t pgsql yum -y groupinstall "Development Tools"
 # Install Oracle Instant Client and SDK
 ### This is a manual step if instantclient-basic-linux.x64-11.2.0.4.0.zip is not already downloaded
 ### Get Instant Client from http://www.oracle.com/technetwork/topics/linuxx86-64soft-092277.html
-# unzip /fdw_demo/instantclient-basic-linux.x64-11.2.0.4.0.zip
-# unzip /fdw_demo/instantclient-sdk-linux.x64-11.2.0.4.0.zip
+# unzip ./instantclient-basic-linux.x64-11.2.0.4.0.zip
+# unzip ./instantclient-sdk-linux.x64-11.2.0.4.0.zip
 docker exec -t pgsql mkdir -p /u01/app/oracle/product/11.2.0/xe/lib
 docker exec -t pgsql bash --login -c "cp /fdw_demo/instantclient_11_2/lib* /u01/app/oracle/product/11.2.0/xe/lib"
 docker exec -t pgsql mkdir -p mkdir -p /u01/app/oracle/product/11.2.0/xe/rdbms/public
 docker exec -t pgsql bash --login -c "cp /fdw_demo/instantclient_11_2/sdk/include/*.h /u01/app/oracle/product/11.2.0/xe/rdbms/public"
+docker exec -t pgsql bash --login -c "echo 'export ORACLE_HOME=/u01/app/oracle/product/11.2.0/xe' >> /etc/profile"
+docker exec -t pgsql bash --login -c "echo 'export PATH=\$ORACLE_HOME/bin:\$PATH' >> /etc/profile"
+docker exec -t pgsql bash --login -c "echo 'export ORACLE_SID=XE' >> /etc/profile"
+docker exec -t pgsql bash --login -c "echo 'export LD_LIBRARY_PATH=\$ORACLE_HOME/lib:\$LD_LIBRARY_PATH' >> /etc/profile"
 docker exec -t pgsql bash --login -c "echo 'export ORACLE_HOME=/u01/app/oracle/product/11.2.0/xe' >> /etc/bashrc"
 docker exec -t pgsql bash --login -c "echo 'export PATH=\$ORACLE_HOME/bin:\$PATH' >> /etc/bashrc"
 docker exec -t pgsql bash --login -c "echo 'export ORACLE_SID=XE' >> /etc/bashrc"
+docker exec -t pgsql bash --login -c "echo 'export LD_LIBRARY_PATH=\$ORACLE_HOME/lib:\$LD_LIBRARY_PATH' >> /etc/bashrc"
 
 # XXX Yes, this is very ugly...
 docker exec -t pgsql ln -s /u01/app/oracle/product/11.2.0/xe/lib/libclntsh.so.11.1 /u01/app/oracle/product/11.2.0/xe/lib/libclntsh.so
