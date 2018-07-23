2f644beb2473de56724a0b0ad1d183090e13060c
Richard Yen <richyen3@gmail.com>
2018-07-23 00:20:32 -0700

    Script to set up PG on ubuntu

---
+++ vm/vagrant/pg_setup_ubuntu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export PGDATA="/etc/postgresql/10/main"
+
+# Install dependencies
+apt-get -y -qq install ca-certificates
+
+# Install Postgres
+echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo apt-get -y -qq update
+sudo apt-get -y -qq upgrade
+sudo apt-get -y -qq install postgresql-10
+sed -i -e "s/peer/trust/" ${PGDATA}/pg_hba.conf
+sudo -u postgres psql -tc "select pg_reload_conf()"
+
+# Done
+echo "Your Vagrant box is all set up.  Use 'vagrant ssh' to log in"
