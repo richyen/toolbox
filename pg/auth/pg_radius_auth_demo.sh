4ef74dacc819c1a9438a21237037e82afa804af2
Richard Yen <richyen3@gmail.com>
2019-04-08 12:11:16 -0700

    Moving stuff not EDB-specific up to `pg/` folder

--- pg/auth/pg_radius_auth_demo.sh
+++ pg/auth/pg_radius_auth_demo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Demo script to set up RADIUS authentication on EPAS 9.x on Centos 6
+# Demo script to set up RADIUS authentication on PG 9.x on Centos 6
 # If using a later version of CentOS, you may need to use /etc/raddb/mods-config/files/authorize
 # Derived from steps shared by Gowtham Kumar
 
@@ -15,7 +15,7 @@ printf "steve Cleartext-Password := \"testing\" \n
               Framed-Routing = Broadcast-Listen, \n
               Framed-Filter-Id = \"std.ppp\", \n
               Framed-MTU = 1500, \n
-              Framed-Compression = Van-Jacobsen-TCP-IP\n" >> /etc/raddb/users 
+              Framed-Compression = Van-Jacobsen-TCP-IP\n" >> /etc/raddb/users
 /etc/init.d/radiusd start
 radtest steve testing 127.0.0.1 0 testing123
 
@@ -23,11 +23,11 @@ radtest steve testing 127.0.0.1 0 testing123
 # test on postgres
 
 psql -c "CREATE USER steve WITH PASSWORD 'foobar'"
-sed -i "s/host   all.*trust/host   all         all      0.0.0.0\/0  radius  radiusserver=127.0.0.1 radiussecret=testing123 radiusport=1812/" /var/lib/ppas/9.5/data/pg_hba.conf
+sed -i "s/host   all.*trust/host   all         all      0.0.0.0\/0  radius  radiusserver=127.0.0.1 radiussecret=testing123 radiusport=1812/" /var/lib/pgsql/9.5/data/pg_hba.conf
 psql -c "select pg_reload_conf()"
- 
+
 # Log in with wrong RADIUS password
 set -x
-PGPASSWORD='foobar' psql -d edb -U steve -h 127.0.0.1
+PGPASSWORD='foobar' psql -d postgres -U steve -h 127.0.0.1
 # Log in with correct RADIUS password
-PGPASSWORD='testing' psql -d edb -U steve -h 127.0.0.1
+PGPASSWORD='testing' psql -d postgres -U steve -h 127.0.0.1
