1a2ec0c33d5df2b56338c47274faf9452b26d531
Richard Yen <richyen3@gmail.com>
2019-04-17 17:45:48 -0700

    update for oracle 12c oracle 11g XE has 11GB size limit, which makes it hard to run benchmark

--- pg/edb/mtk/clob_benchmark/run_test.sh
+++ pg/edb/mtk/clob_benchmark/run_test.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 echo "tests starting at `date`"
-increment=500000
-for rs in 1000 2000 4000 8000 16000
+#increment=500000
+increment=5
+#for rs in 1000 2000 4000 8000 16000
+for rs in 1000
 do
   echo "row size $rs starting at `date`"
-  docker exec -t ora_host bash --login -c "sqlplus -S system/oracle < /docker/deleterows.sql"
-  for nr in `seq 500000 ${increment} 5000000`
+  docker exec -t ora_host bash --login -c "echo 'truncate table hr.foolob' | sqlplus -S sys/Oradoc_db1 as sysdba"
+  #for nr in `seq 500000 ${increment} 5000000`
+  for nr in `seq 5 ${increment} 30`
   do
     echo "inner loop $nr/$rs starting at `date`"
     docker exec -it pg_host  psql -c "drop schema hr cascade"
     echo "incrementing $increment for row size $rs at `date`"
-    docker exec -t  ora_host bash --login -c "/docker/add_data.sh ${increment} ${rs}"
+    docker exec -t ora_host bash --login -c "echo 'execute HR.my_proc(${increment},${rs})' | sqlplus -S sys/Oradoc_db1 as sysdba"
     docker exec -it -u enterprisedb pg_host bash --login -c "/usr/edb/migrationtoolkit/bin/runMTK.sh -copyViaDBLinkOra -tables FOOLOB -replaceNullChar x -verbose on HR"
     docker exec -it pg_host psql -c "select count(*) from hr.foolob"
     echo "inner loop $nr/$rs finished at `date`"
--- pg/edb/mtk/clob_benchmark/setup.sh
+++ pg/edb/mtk/clob_benchmark/setup.sh
@@ -2,14 +2,20 @@
 
 # SETUP INSTRUCTIONS
 # 1. docker-compoase up -d
-# 2. set ORACLE_HOME, PATH, ORACLE_SID environment variables in ora_host container
 # 3. stop edb-as-11 in pg_host container
 # 4. log into pg_host container as enterprisedb and start manually with pg_ctl (so it picks up the ORACLE environment variables)
 # 5. run ora_setup.sql to create tables
 
-docker exec -t ora_host bash --login -c "echo 'export ORACLE_HOME=/u01/app/oracle/product/11.2.0/xe' >> /etc/profile"
-docker exec -t ora_host bash --login -c "echo 'export PATH=\$ORACLE_HOME/bin:\$PATH' >> /etc/profile"
-docker exec -t ora_host bash --login -c "echo 'export ORACLE_SID=XE' >> /etc/profile"
-docker exec -t ora_host bash --login -c "sqlplus -S system/oracle < /docker/ora_setup.sql"
+#docker-compose down
+#docker-compose up -d
 
-docker exec -t pg_host bash --login -c "chown enterprisedb:enterprisedb /usr/edb/migrationtoolkit/etc/toolkit.properties"
+docker exec -u enterprisedb -t pg_host bash --login -c "pg_ctl -D /var/lib/edb/as11/data -mf stop"
+docker exec -u enterprisedb -t pg_host bash --login -c "pg_ctl -D /var/lib/edb/as11/data -l /tmp/logfile start"
+
+while [[ `docker ps -a | grep ora_host | grep -c healthy` -ne 1 ]]
+do
+  echo "waiting for ora_host to come up"
+  sleep 1
+done
+
+docker exec -t ora_host bash --login -c "sqlplus -S sys/Oradoc_db1 as sysdba < /docker/ora_setup.sql"
