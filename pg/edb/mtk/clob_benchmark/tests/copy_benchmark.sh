50de2b98d2433ab54513a670ce6d510f26e86d5f
Richard Yen <richyen3@gmail.com>
2019-04-22 14:58:17 -0700

    calculate range based on calculated chunk size, rather than just hope that seq will figure it out

--- pg/edb/mtk/clob_benchmark/tests/copy_benchmark.sh
+++ pg/edb/mtk/clob_benchmark/tests/copy_benchmark.sh
@@ -4,20 +4,21 @@
 
 query="CREATE TABLE IF NOT EXISTS public.foolob_new ( n numeric, c clob )"
 psql -Atc "${query}"
-for incsize in `seq 5 25`
+for num_threads in `seq 1 24`
 do
-  echo "starting $incsize at `date`" >> /tmp/times.log
   query="TRUNCATE TABLE public.foolob_new"
   psql -Atc "${query}"
-  for start in `seq 0 ${incsize}00000 24000000`
+  segment_size=$(( 24000000 / num_threads ))
+  echo "starting $num_threads at `date`" >> /tmp/times.log
+  for thread_num in `seq 0 $(( num_threads - 1 ))`
   do
-    query="select dblink_ora_connect('my_testlink','ora_host','ORCLCDB.localdomain','HR','HR',1521,false); SELECT dblink_ora_copy('my_testlink','select n,c from (select a.*, rownum rnum from (select n,c FROM foolob order by n) a where rownum <= $(( start + 999999 ))) where rnum >= ${start}', 'public', 'foolob_new')"
+    query="select dblink_ora_connect('my_testlink','ora_host','ORCLCDB.localdomain','HR','HR',1521,false); SELECT dblink_ora_copy('my_testlink','select n,c from (select a.*, rownum rnum from (select n,c FROM foolob order by n) a where rownum <= $(( thread_num * segment_size + (segment_size - 1)))) where rnum >= $(( thread * segment_size ))', 'public', 'foolob_new')"
     psql -Atc "${query}" &
-    pids[${start}]=$!
+    pids[${thread_num}]=$!
   done
   for pid in ${pids[*]}; do
     wait $pid
   done
   psql -c "select count(*) from public.foolob_new" >> /tmp/times.log
-  echo "done with $incsize at `date`" >> /tmp/times.log
+  echo "done with $thread_size at `date`" >> /tmp/times.log
 done
