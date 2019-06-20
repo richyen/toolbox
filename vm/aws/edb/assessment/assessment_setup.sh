b22a95f7c12bf250b486b1e13fa57b301c956995
Richard Yen <richyen3@gmail.com>
2019-06-20 09:51:35 -0700

    update for newer versions of EDBAS

--- vm/aws/edb/assessment/assessment_setup.sh
+++ vm/aws/edb/assessment/assessment_setup.sh
@@ -2,21 +2,26 @@
 
 ### Provision a VM for assessment
 
+### TODO: Set these accordingly
+YUMUSERNAME=##########
+YUMPASSWORD=##########
+
+# Take in env variable, or use commandline arg
+: "${SSH_PUB_KEY:=$1}"
+: "${SSH_PUB_KEY:?Need to set SSH_PUB_KEY}"
+echo "${SSH_PUB_KEY}"
+
 ### Environment
-REPONAME=ppas95
-PGMAJOR=9.5
+PGMAJOR=11
 PGPORT=5432
 PGDATABASE=edb
 PGUSER=enterprisedb
-PGHOME=/home/postgres
-PATH=/usr/ppas-${PGMAJOR}/bin:${PATH}
-PGDATA=/var/lib/ppas/${PGMAJOR}/data
-PGLOG=/var/lib/ppas/${PGMAJOR}/pgstartup.log
-
-### TODO: Set these accordingly
-YUMUSERNAME=######
-YUMPASSWORD=######
-SSH_PUB_KEY="######"
+PGHOME=/var/lib/edb
+PGBIN=/usr/edb/as${PGMAJOR}/bin
+PATH=${PGBIN}:${PATH}
+PGDATA=${PGHOME}/as${PGMAJOR}/data
+PGLOG=${PGHOME}/as${PGMAJOR}/pgstartup.log
+REPONAME=edbas${PGMAJOR}
 
 ### Monitor command history
 echo "export HISTTIMEFORMAT=\"%Y-%m-%d %T \"" >> /etc/bashrc
@@ -27,12 +32,22 @@ rpm -ivh http://yum.enterprisedb.com/edbrepos/edb-repo-latest.noarch.rpm
 sed -i "s/<username>:<password>/${YUMUSERNAME}:${YUMPASSWORD}/" /etc/yum.repos.d/edb.repo
 yum -y update
 yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-yum -y --enablerepo=${REPONAME} --enablerepo=enterprisedb-tools --enablerepo=enterprisedb-dependencies install ${REPONAME}-server.x86_64 sudo wget edb-jdbc java-1.7.0-openjdk-devel
+yum -y --enablerepo=${REPONAME} --enablerepo=enterprisedb-tools --enablerepo=enterprisedb-dependencies install edb-as${PGMAJOR}-server.x86_64 sudo wget edb-jdbc java-1.7.0-openjdk-devel
+
+### Prepare user
+mkdir ${PGHOME}/.ssh
+echo ${SSH_PUB_KEY} >> ${PGHOME}/.ssh/authorized_keys
+chmod 700 ${PGHOME}/.ssh
+chmod 600 ${PGHOME}/.ssh/authorized_keys
+chown -R ${PGUSER}:${PGUSER} ${PGHOME}
+restorecon -r -vv ${PGHOME}/.ssh    ### See https://ubuntuforums.org/showthread.php?t=1932058&s=00b938a71d52ba6c47b581befd8e71f6&p=12472161#post12472161
+echo "${PGUSER}    ALL=(ALL)   NOPASSWD: ALL" >> /etc/sudoers
+echo "export PATH=${PATH}" >> /etc/profile
 
 ### Initialize new database
 rm -rf ${PGDATA}
-sudo -u ${PGUSER} /usr/ppas-${PGMAJOR}/bin/initdb -D ${PGDATA}
-sed -i "s/^PGPORT.*/PGPORT=${PGPORT}/" /etc/sysconfig/ppas/ppas-${PGMAJOR}
+sudo -u ${PGUSER} ${PGBIN}/initdb -D ${PGDATA}
+sed -i "s/^PGPORT.*/PGPORT=${PGPORT}/" /etc/sysconfig/edb/as${PGMAJOR}
 echo "export PGPORT=${PGPORT}"         >> /etc/profile.d/pg_env.sh
 echo "export PGDATABASE=${PGDATABASE}" >> /etc/profile.d/pg_env.sh
 echo "export PGUSER=${PGUSER}"         >> /etc/profile.d/pg_env.sh
@@ -42,12 +57,8 @@ echo "host   all         all      0.0.0.0/0  trust" >> ${PGDATA}/pg_hba.conf
 mkdir ${PGDATA}/pg_log
 chown ${PGUSER}:${PGUSER} ${PGDATA}/pg_log
 
-### Set user info
-adduser --home-dir ${PGHOME} --create-home postgres
-echo 'postgres    ALL=(ALL)   NOPASSWD: ALL' >> /etc/sudoers
-
 ### Start EDBAS
-systemctl start ppas-${PGMAJOR}
+sudo -iu ${PGUSER} ${PGINSTALL}/pg_ctl -D ${PGDATA} -l ${PGDATA}/logfile start
 
 ### Prepare for assessment
 mkdir ${PGHOME}/.ssh
@@ -61,6 +72,6 @@ wget -P ${PGHOME} "https://raw.githubusercontent.com/richyen/toolbox/master/vm/a
 wget -P ${PGHOME} "https://raw.githubusercontent.com/richyen/toolbox/master/vm/aws/edb/assessment/top_performers.sql"
 wget -P ${PGHOME} "https://raw.githubusercontent.com/richyen/toolbox/master/vm/aws/edb/assessment/update_data.sh"
 cp /usr/edb/connectors/jdbc/edb-jdbc17.jar ${PGHOME}
-chown -R postgres:postgres ${PGHOME}
+chown -R ${PGUSER}:${PGUSER} ${PGHOME}
 
 rm -f assessment_vm.sh
