a4a6aaa40da305ea8025f72c76de97bddef0fdad
Richard Yen <richyen3@gmail.com>
2019-09-13 10:32:50 -0700

    use interpolation, and make variables more easy to follow

--- pg/replication/streaming_replication_tests/max_standby_streaming_delay_test.sh
+++ pg/replication/streaming_replication_tests/max_standby_streaming_delay_test.sh
@@ -2,46 +2,56 @@
 
 # This script demonstrates the effect of tuning max_standby_streaming_delay, and how it cancels long-running queries that interfere with WAL-writing (specifically, maintenance)
 
-# This script to be run on the standby server after a Streaming Replication cluster is set up
-MASTER_IP=172.17.0.6
+# NOTE: This script to be run on the standby server; it is up to you to set up streaming replication first
+
+MASTER_IP=127.0.0.1
+MASTER_PGPORT=5432
+STANDBY_IP=`hostname -i`
+STANDBY_PGPORT=5433
+STANDBY_PGDATA="/tmp/data"
+sleep_time=120
 
 # Setup
-dropdb -h ${MASTER_IP} bench
-createdb -h ${MASTER_IP} bench
-pgbench -i -F 50 -s 50 -h ${MASTER_IP} bench 2> /dev/null
+dropdb -h ${MASTER_IP} -p ${MASTER_PGPORT} bench
+createdb -h ${MASTER_IP} -p ${MASTER_PGPORT} bench
+pgbench -i -F 50 -s 50 -h ${MASTER_IP} -p ${MASTER_PGPORT} bench 2> /dev/null
 
 # See if max_standby_streaming_delay cancels a query
-sed -i "s/max_standby_streaming_delay.*/max_standby_streaming_delay = 10s/" /var/lib/ppas/9.5/data/postgresql.conf
-psql -Atc "select pg_reload_conf()"
-psql -Atc "show max_standby_streaming_delay"
-psql -h ${MASTER_IP} -c "DELETE FROM pgbench_accounts where aid between 0 and 10000" bench
-psql -c "select pg_sleep(45), count(*) from pgbench_accounts" bench &
-psql -h ${MASTER_IP} -c "vacuum" bench
-sleep 120 # Wait for pg_sleep to get canceled
+sed -i "s/max_standby_streaming_delay.*/max_standby_streaming_delay = 10s/" ${STANDBY_PGDATA}/postgresql.conf
+psql -h ${STANDBY_IP} -p ${STANDBY_PGPORT} -Atc "select pg_reload_conf()"
+psql -h ${STANDBY_IP} -p ${STANDBY_PGPORT} -Atc "show max_standby_streaming_delay"
+psql -h ${MASTER_IP}  -p ${MASTER_PGPORT} -c "DELETE FROM pgbench_accounts where aid between 0 and 10000" bench
+psql -h ${STANDBY_IP} -p ${STANDBY_PGPORT} -c "select pg_sleep(45), count(*) from pgbench_accounts" bench &
+psql -h ${MASTER_IP}  -p ${MASTER_PGPORT} -c "vacuum" bench
+echo "You should see a query cancellation message soon"
+sleep ${sleep_time}
 
 # Try again
-sed -i "s/max_standby_streaming_delay.*/max_standby_streaming_delay = 60s/" /var/lib/ppas/9.5/data/postgresql.conf
-psql -Atc "select pg_reload_conf()"
-psql -Atc "show max_standby_streaming_delay"
-psql -h ${MASTER_IP} -c "DELETE FROM pgbench_accounts where aid between 10000 and 20000" bench
-psql -c "select pg_sleep(30), count(*) from pgbench_accounts" bench &
-psql -h ${MASTER_IP} -c "vacuum" bench
-sleep 120 # This pg_sleep should succeed
+sed -i "s/max_standby_streaming_delay.*/max_standby_streaming_delay = 60s/" ${STANDBY_PGDATA}/postgresql.conf
+psql -h ${STANDBY_IP} -p ${STANDBY_PGPORT} -Atc "select pg_reload_conf()"
+psql -h ${STANDBY_IP} -p ${STANDBY_PGPORT} -Atc "show max_standby_streaming_delay"
+psql -h ${MASTER_IP}  -p ${MASTER_PGPORT} -c "DELETE FROM pgbench_accounts where aid between 10000 and 20000" bench
+psql -h ${STANDBY_IP} -p ${STANDBY_PGPORT} -c "select pg_sleep(30), count(*) from pgbench_accounts" bench &
+psql -h ${MASTER_IP}  -p ${MASTER_PGPORT} -c "vacuum" bench
+echo "No query cancellation should occur in the next ${sleep_time} seconds"
+sleep ${sleep_time}
 
 # Try with longer sleep
-psql -h ${MASTER_IP} -c "DELETE FROM pgbench_accounts where aid between 20000 and 30000" bench
-psql -c "select pg_sleep(120), count(*) from pgbench_accounts" bench &
-psql -h ${MASTER_IP} -c "vacuum" bench
-sleep 120 # This pg_sleep should get canceled
+psql -h ${MASTER_IP}  -p ${MASTER_PGPORT} -c "DELETE FROM pgbench_accounts where aid between 20000 and 30000" bench
+psql -h ${STANDBY_IP} -p ${STANDBY_PGPORT} -c "select pg_sleep(120), count(*) from pgbench_accounts" bench &
+psql -h ${MASTER_IP}  -p ${MASTER_PGPORT} -c "vacuum" bench
+echo "You should see a query cancellation message soon"
+sleep ${sleep_time}
 
 # Try again
-sed -i "s/max_standby_streaming_delay.*/max_standby_streaming_delay = 120s/" /var/lib/ppas/9.5/data/postgresql.conf
-psql -Atc "select pg_reload_conf()"
-psql -Atc "show max_standby_streaming_delay"
-psql -h ${MASTER_IP} -c "DELETE FROM pgbench_accounts where aid between 30000 and 40000" bench
-psql -c "select pg_sleep(60), count(*) from pgbench_accounts" bench &
-psql -h ${MASTER_IP} -c "vacuum" bench
-sleep 120 # This pg_sleep should succeed
+sed -i "s/max_standby_streaming_delay.*/max_standby_streaming_delay = 120s/" ${STANDBY_PGDATA}/postgresql.conf
+psql -h ${STANDBY_IP} -p ${STANDBY_PGPORT} -Atc "select pg_reload_conf()"
+psql -h ${STANDBY_IP} -p ${STANDBY_PGPORT} -Atc "show max_standby_streaming_delay"
+psql -h ${MASTER_IP}  -p ${MASTER_PGPORT} -c "DELETE FROM pgbench_accounts where aid between 30000 and 40000" bench
+psql -h ${STANDBY_IP} -p ${STANDBY_PGPORT} -c "select pg_sleep(60), count(*) from pgbench_accounts" bench &
+psql -h ${MASTER_IP}  -p ${MASTER_PGPORT} -c "vacuum" bench
+echo "No query cancellation should occur in the next ${sleep_time} seconds"
+sleep ${sleep_time}
 
 # Check the number of conflicts recorded
-psql -c "select * from pg_stat_database_conflicts where datname = 'bench'"
+psql -h ${STANDBY_IP} -p ${STANDBY_PGPORT} -c "select * from pg_stat_database_conflicts where datname = 'bench'"
