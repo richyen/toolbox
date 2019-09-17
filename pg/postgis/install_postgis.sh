ee08de4f2383f34bc97aa0ae1c5b4e21408428cf
Richard Yen <richyen3@gmail.com>
2019-09-16 23:56:28 -0700

    support EDB Advanced Server

--- pg/postgis/install_postgis.sh
+++ pg/postgis/install_postgis.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
-# First, do docker-compose up -d on a centos7/postgres-10:latest image
-
 PGVERNUM=10
+USE_EDB=0
 
 yum -y install epel-release
-yum -y install postgresql${PGVERNUM}-contrib.x86_64 postgis24_${PGVERNUM}
+if [[ ${USE_EDB} -eq 1 ]]
+then
+  yum -y --enablerepo=enterprisedb-tools --enablerepo=enterprisedb-dependencies --enablerepo=edbas${PGVERNUM} install edb-as${PGVERNUM}-postgis-core.x86_64
+  if [[ `cat /etc/*release* | grep "^cpe" | cut -f5 -d ':'` -eq 7 ]]
+    systemctl start edb-as-${PGVERNUM}
+  fi
+else
+  yum -y install postgresql${PGVERNUM}-contrib.x86_64 postgis24_${PGVERNUM}
+  if [[ `cat /etc/*release* | grep "^cpe" | cut -f5 -d ':'` -eq 7 ]]
+    systemctl start postgresql-${PGVERNUM}
+  fi
+fi
 
-systemctl start postgresql-10
 
-psql -c "CREATE EXTENSION postgis" template1 postgres
-psql -c "CREATE EXTENSION postgis_topology" template1 postgres
-psql -c "CREATE EXTENSION fuzzystrmatch" template1 postgres
-psql -c "CREATE EXTENSION postgis_tiger_geocoder" template1 postgres
+psql -c "CREATE EXTENSION postgis" template1
+psql -c "CREATE EXTENSION postgis_topology" template1
+psql -c "CREATE EXTENSION fuzzystrmatch" template1
+psql -c "CREATE EXTENSION postgis_tiger_geocoder" template1
 
-# createdb -U postgres postgis_test
+createdb postgis_test
+
+psql -c "select PostGIS_full_version()" postgis_test
