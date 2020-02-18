dfeef705cfda369536f6cfd5a77158c55fcf6b2d
Richard Yen <richyen3@gmail.com>
2020-02-17 23:42:42 -0800

    Install with PGDG RPM

--- pg/replication/slony/scripts/0_slony_install.sh
+++ pg/replication/slony/scripts/0_slony_install.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-#export YUMUSERNAME=''
-#export YUMPASSWORD=''
-
-rpm -ivh http://yum.enterprisedb.com/edbrepos/edb-repo-latest.noarch.rpm
-sed -i "s/<username>:<password>/${YUMUSERNAME}:${YUMPASSWORD}/" /etc/yum.repos.d/edb.repo
-yum -y install yum-plugin-ovl
-yum -y install epel-release perl-DBD-Pg
-
 ### To use EDB RPM
-yum -y --enablerepo=edbas10 --enablerepo=enterprisedb-tools --enablerepo=enterprisedb-dependencies install edb-as10-server edb-as10-slony-replication
+# export YUMUSERNAME=''
+# export YUMPASSWORD=''
+# rpm -ivh http://yum.enterprisedb.com/edbrepos/edb-repo-latest.noarch.rpm
+# sed -i "s/<username>:<password>/${YUMUSERNAME}:${YUMPASSWORD}/" /etc/yum.repos.d/edb.repo
+# yum -y install yum-plugin-ovl
+# yum -y install epel-release perl-DBD-Pg
+# yum -y --enablerepo=edbas10 --enablerepo=enterprisedb-tools --enablerepo=enterprisedb-dependencies install edb-as10-server edb-as10-slony-replication
+
+### To use PGDG RPM
+export PGVERSION=11
+yum -y install slony1-${PGVERSION}
 
 ### To compile from source
 # SLON_VER=2.2
-# PG_BIN=/usr/ppas-9.5/bin
+# PG_BIN=/usr/pgsql-${PGVERSION}/bin
 # yum -y install wget tar bzip2 gcc
 # wget http://main.slony.info/downloads/${SLON_VER}/source/slony1-${SLON_VER}.4.tar.bz2
 # tar -xvjf slony1-${SLON_VER}.4.tar.bz2
