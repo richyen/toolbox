6a908a7f82e31626b60a80398f0093fe6acbe1c2
Richard Yen <richyen3@gmail.com>
2020-04-24 10:57:24 -0700

    refactor for simplicity; use unified Yum URL

--- vm/docker/postgresql/build_dockerfile.sh
+++ vm/docker/postgresql/build_dockerfile.sh
@@ -2,33 +2,33 @@
 
 if [[ "x${1}" == "x" ]]
 then
-  echo "Usage: ${0} <postgres_major_version> <do_build>"
+  echo "Usage: ${0} <postgres_major_version> [<do_build>]"
+  exit 1
 fi
 
+CENTOSVER=7
 PGMAJOR=${1}
-PGVERNUM=`echo ${PGMAJOR} | sed "s/\.//g"`
-# RPM_URL=`curl -s https://yum.postgresql.org/repopackages.php | grep ${PGMAJOR} | grep https | grep "CentOS 6" | grep x86_64 | sed "s/.*https/https/" | sed "s/rpm.*/rpm/"`
-RPM_URL="https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm"
+RPM_URL="https://download.postgresql.org/pub/repos/yum/reporpms/EL-${CENTOSVER}-x86_64/pgdg-redhat-repo-latest.noarch.rpm"
 
-mkdir -p ${PGMAJOR}
-cp Dockerfile.template ${PGMAJOR}/Dockerfile
-
-sed -i "s!^ENV PGMAJOR.*!ENV PGMAJOR=${PGMAJOR}!" ${PGMAJOR}/Dockerfile
-sed -i "s!^ENV PGVERNUM.*!ENV PGVERNUM=${PGVERNUM}!" ${PGMAJOR}/Dockerfile
-sed -i "s!^ENV RPM_URL.*!ENV RPM_URL=${RPM_URL}!" ${PGMAJOR}/Dockerfile
+cp Dockerfile.template Dockerfile
 
 # For some reason, sometimes this is needed, but sometimes not (maybe depending on the mirror, the paths might be different)
-# if [[ ${PGVERNUM} -lt 93 ]]
+# if [[ ${PGMAJOR/./} -lt 93 ]]
 # then
-#   sed -i "s/postgresql-\${PGMAJOR}/postgresql/" ${PGMAJOR}/Dockerfile
-#   sed -i "s!PGDATA=/var/lib/pgsql/\${PGMAJOR}/data!PGDATA=/var/lib/pgsql/data!" ${PGMAJOR}/Dockerfile
-#   sed -i "s!PGLOG=/var/lib/pgsql/\${PGMAJOR}/pgstartup.log!PGLOG=/var/lib/pgsql/pgstartup.log!" ${PGMAJOR}/Dockerfile
+#   sed -i -e "s/postgresql-\${PGMAJOR}/postgresql/" \
+#          -e "s!PGDATA=/var/lib/pgsql/\${PGMAJOR}/data!PGDATA=/var/lib/pgsql/data!" \
+#          -e "s!PGLOG=/var/lib/pgsql/\${PGMAJOR}/pgstartup.log!PGLOG=/var/lib/pgsql/pgstartup.log!" Dockerfile
 # fi
 
-cd ${PGMAJOR}
+if [[ ${CENTOSVER} -eq 7 ]]
+then
+  sed -i -e "s/^FROM.*/FROM gisjedi\/gosu-centos/" \
+         -e "s/^#RUN gosu/RUN gosu/" \
+         -e "s/^CMD.*/CMD tail -F \/var\/log\/yum.log/" Dockerfile
+fi
 
 if [[ "${2}" == "do_build" ]]
 then
-#  docker build --build-arg PGMAJOR=${PGMAJOR} --build-arg PGVERNUM=${PGVERNUM} --build-arg RPM_URL=${RPM_URL} -t postgres-${PGMAJOR}:latest .
-  docker build -t postgres-${PGMAJOR}:latest .
+  docker build --build-arg PGMAJOR=${PGMAJOR} --build-arg RPM_URL=${RPM_URL} -t postgres-${PGMAJOR}:latest .
+  rm -f Dockerfile
 fi
