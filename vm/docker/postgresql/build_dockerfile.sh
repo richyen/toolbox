27634aab5b8d4dcaf10e391e758b8b4f7838513f
Richard Yen <richyen3@gmail.com>
2020-04-24 11:38:51 -0700

    macos sed has strange behavior with multiline sed and -i; add ability to push to local registry upon build

--- vm/docker/postgresql/build_dockerfile.sh
+++ vm/docker/postgresql/build_dockerfile.sh
@@ -6,29 +6,44 @@ then
   exit 1
 fi
 
-CENTOSVER=7
+CENTOSVER=6
 PGMAJOR=${1}
 RPM_URL="https://download.postgresql.org/pub/repos/yum/reporpms/EL-${CENTOSVER}-x86_64/pgdg-redhat-repo-latest.noarch.rpm"
 
-cp Dockerfile.template Dockerfile
 
 # For some reason, sometimes this is needed, but sometimes not (maybe depending on the mirror, the paths might be different)
 # if [[ ${PGMAJOR/./} -lt 93 ]]
 # then
-#   sed -i -e "s/postgresql-\${PGMAJOR}/postgresql/" \
-#          -e "s!PGDATA=/var/lib/pgsql/\${PGMAJOR}/data!PGDATA=/var/lib/pgsql/data!" \
-#          -e "s!PGLOG=/var/lib/pgsql/\${PGMAJOR}/pgstartup.log!PGLOG=/var/lib/pgsql/pgstartup.log!" Dockerfile
+#   sed -e "s/postgresql-\${PGMAJOR}/postgresql/" \
+#       -e "s!PGDATA=/var/lib/pgsql/\${PGMAJOR}/data!PGDATA=/var/lib/pgsql/data!" \
+#       -e "s!PGLOG=/var/lib/pgsql/\${PGMAJOR}/pgstartup.log!PGLOG=/var/lib/pgsql/pgstartup.log!" -i Dockerfile
 # fi
 
 if [[ ${CENTOSVER} -eq 7 ]]
 then
-  sed -i -e "s/^FROM.*/FROM gisjedi\/gosu-centos/" \
-         -e "s/^#RUN gosu/RUN gosu/" \
-         -e "s/^CMD.*/CMD tail -F \/var\/log\/yum.log/" Dockerfile
+  sed -e "s/^FROM.*/FROM gisjedi\/gosu-centos/" \
+      -e "s/^#RUN gosu/RUN gosu/" \
+      -e "s/^RUN service/#RUN service/" \
+      -e "s/^CMD.*/CMD tail -F \/var\/log\/yum.log/" Dockerfile.template > Dockerfile
+else
+  cp Dockerfile.template Dockerfile
 fi
 
 if [[ "${2}" == "do_build" ]]
 then
-  docker build --build-arg PGMAJOR=${PGMAJOR} --build-arg RPM_URL=${RPM_URL} -t postgres-${PGMAJOR}:latest .
+  # Assume this is the latest version
+  docker build --build-arg PGMAJOR=${PGMAJOR} --build-arg RPM_URL=${RPM_URL} -t centos${CENTOSVER}/postgres:latest .
+
+  # Give the image an actual version tag
+  VN=`docker run -it --rm centos${CENTOSVER}/postgres:latest psql --version | awk '{ print \$3 }'| tr -d '\r'`
+  docker tag centos${CENTOSVER}/postgres:latest centos${CENTOSVER}/postgres:${VN}
+
+  if [[ "${3}x" != "x" ]]
+  then
+    # Update registry too
+    docker tag centos${CENTOSVER}/postgres:${VN} ${3}/centos${CENTOSVER}/postgres:${VN}
+    docker push ${3}/centos${CENTOSVER}/postgres:${VN}
+  fi
+
   rm -f Dockerfile
 fi
