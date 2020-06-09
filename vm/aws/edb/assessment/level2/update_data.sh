c67bc8f9747b6e2c3cef4f558e4113b996d55941
Richard Yen <richyen3@gmail.com>
2020-06-09 00:35:18 -0700

    push L2

---
+++ vm/aws/edb/assessment/level2/update_data.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+export PGPORT=5432
+export PGUSER=enterprisedb
+export PGDATABASE=edb
+export PGHOST=127.0.0.1
+
+outfile="$( date +%s )_update_data.txt"
+D1=`date`
+
+# Give everyone a 10% raise
+for empno in `psql -Atc "SELECT empno FROM emp"`
+  do
+  psql -c "UPDATE emp SET sal = sal * 1.1 WHERE empno = ${empno}"
+done
+
+# Populate the sales table
+psql -c "INSERT INTO sales VALUES (generate_series(1001,20000),(SELECT max(empno) FROM emp),md5(random()), now() - random() * interval '1 year', (random()*100)::int);"
+psql -c "UPDATE sales SET salesperson = e.empno, sale_amount = random() * 100 FROM (select distinct (empno % 13) + 1 as idx, * from emp) e WHERE (sales.id % 13) = e.idx"
+
+# Set the customer name for each row based on the empno/salesperson
+MAXID=`psql -Atc "SELECT max(id) FROM sales"`
+for id in `seq 1 ${MAXID}`
+  do
+  psql -qc "UPDATE sales SET customer_name = (SELECT 'Customer of ' || empno FROM emp WHERE empno = sales.salesperson)"
+done
+
+D2=`date`
+echo "Start time: ${D1}" | tee -a $outfile
+echo "End time: ${D2}" | tee -a $outfile
