38e81f9ecaa65aa58dedb03cc4c5f942409a98f5
Richard Yen <richard.yen@enterprisedb.com>
2021-04-22 21:51:26 -0700

    Polish up steps, remove race conditions

--- pg/replication/pglogical/entrypoint.sh
+++ pg/replication/pglogical/entrypoint.sh
@@ -5,10 +5,12 @@
 # Environment vars
 PROVIDER_IP=10.111.211.11
 SUBSCRIBER_IP=10.111.211.12
+REPDB=repdb
+PGUSER=postgres
 
 # Make sure environment is clean
-curl https://access.2ndquadrant.com/api/repository/dl/default/release/${PGMAJOR}/rpm | bash
-yum -y install postgresql${PGMAJOR}-pglogical postgresql12-contrib
+curl -s https://access.2ndquadrant.com/api/repository/dl/default/release/${PGMAJOR}/rpm | bash
+yum -y -q install postgresql${PGMAJOR}-pglogical postgresql12-contrib
 
 # Start and configure postgres
 cat << EOF >> /var/lib/pgsql/${PGMAJOR}/data/postgresql.conf
@@ -22,29 +24,35 @@ pglogical.conflict_resolution = 'last_update_wins'
 EOF
 
 su - postgres -c "pg_ctl start"
+createdb ${REPDB}
 psql -c "ALTER SYSTEM SET autovacuum TO off"
-psql -c "CREATE EXTENSION IF NOT EXISTS citext WITH SCHEMA public"
-psql -c "CREATE EXTENSION pglogical"
+psql -c "CREATE EXTENSION IF NOT EXISTS citext WITH SCHEMA public" ${REPDB}
+psql -c "CREATE EXTENSION pglogical" ${REPDB}
 psql -c "SELECT pg_reload_conf()"
 psql -c "SHOW autovacuum"
 
-if [[ $( hostname ) == 'provider' ]]; then
-  psql -c "select pglogical.create_node(node_name := 'provider1', dsn := 'host=${PROVIDER_IP} port=5432 dbname=postgres user=postgres password=postgres')"
-  psql -c "SELECT pglogical.replication_set_add_all_tables('default', ARRAY['public'])"
-else
-  psql -c "SELECT pglogical.create_node( node_name := 'subscriber1', dsn := 'host=${SUBSCRIBER_IP} port=5432 dbname=postgres user=postgres password=postgres' )"
-  psql -c "SELECT pglogical.create_subscription( subscription_name := 'subscription1', provider_dsn := 'host=${PROVIDER_IP} port=5432 dbname=postgres user=postgres password=postgres' )"
+if [[ $( hostname ) == "provider" ]]; then
+  # Create data
+  pgbench -i ${REPDB}
+  psql -c "DROP TABLE pgbench_history" ${REPDB}
+  pg_dump -s ${REPDB} | psql -h ${SUBSCRIBER_IP} ${REPDB}
+
+  # Create replication cluster
+  psql -c "select pglogical.create_node(node_name := 'provider1', dsn := 'host=${PROVIDER_IP} port=5432 dbname=${REPDB} user=${PGUSER} password=postgres')" ${REPDB}
+  psql -c "SELECT pglogical.replication_set_add_all_tables('default', ARRAY['public'])" ${REPDB}
+
+  # Create subscription
+  psql -h ${SUBSCRIBER_IP} -c "SELECT pglogical.create_node( node_name := 'subscriber1', dsn := 'host=${SUBSCRIBER_IP} port=5432 dbname=${REPDB} user=${PGUSER} password=postgres' )" ${REPDB}
+  psql -h ${SUBSCRIBER_IP} -c "SELECT pglogical.create_subscription( subscription_name := 'subscription1', provider_dsn := 'host=${PROVIDER_IP} port=5432 dbname=${REPDB} user=${PGUSER} password=postgres' )" ${REPDB}
+
+  # Check replication status
+  psql -h ${SUBSCRIBER_IP} -c "select subscription_name, status FROM pglogical.show_subscription_status()" ${REPDB}
+  psql -h ${SUBSCRIBER_IP} -c "SELECT pglogical.wait_for_subscription_sync_complete('subscription1')" ${REPDB}
+
+  # Make sure data was replicated
+  psql -c "SELECT count(*) from pgbench_accounts" ${REPDB}
+  psql -h ${SUBSCRIBER_IP} -c "SELECT count(*) from pgbench_accounts" ${REPDB}
 fi
-
-if [[ $( hostname ) == 'provider' ]]; then
-  pgbench -i
-fi
-
-psql -c "select subscription_name, status FROM pglogical.show_subscription_status()"
-psql -c "SELECT pglogical.wait_for_subscription_sync_complete('subscription1')"
-
-# Create target database
-psql -c "SELECT count(*) from pgbench_accounts"
 
 # Keep running (if desired)
 tail -f /dev/null
