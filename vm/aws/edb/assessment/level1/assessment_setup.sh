27c9c0fd2d5ef1b3235849a9ad379015d9793bbc
Richard Yen <richard.yen@enterprisedb.com>
2021-05-03 10:11:36 -0700

    Add docker for L1

--- vm/aws/edb/assessment/level1/assessment_setup.sh
+++ vm/aws/edb/assessment/level1/assessment_setup.sh
@@ -2,10 +2,13 @@
 
 ### Provision a VM for assessment
 
-# Take in env variable, or use commandline arg
-: "${SSH_PUB_KEY:=$1}"
-: "${SSH_PUB_KEY:?Need to set SSH_PUB_KEY}"
-echo "${SSH_PUB_KEY}"
+NODOCKER=1
+if [[ ${NODOCKER} -eq 1 ]]; then
+  # Take in env variable, or use commandline arg
+  : "${SSH_PUB_KEY:=$1}"
+  : "${SSH_PUB_KEY:?Need to set SSH_PUB_KEY}"
+  echo "${SSH_PUB_KEY}"
+fi
 
 ### Environment
 export PGUSER=postgres
@@ -16,23 +19,28 @@ export PGHOME="/var/lib/pgsql"
 export PGINSTALL="/usr/pgsql-${PGVERSION}/bin"
 export PATH="${PGINSTALL}:${PATH}"
 
-### Monitor command history
-echo "export HISTTIMEFORMAT=\"%Y-%m-%d %T \"" >> /etc/bashrc
-echo "PROMPT_COMMAND='history -a >(tee -a ~/.bash_history | logger -t \"\$USER[\$\$] \$SSH_CONNECTION\")'" >> /etc/bashrc
+if [[ ${NODOCKER} -eq 1 ]]; then
+  ### Installation
+  rpm -ivh https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+  yum -q -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+  yum -q -y install vim sudo postgresql${PGVERSION}-server
 
-rpm -ivh https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
-yum -q -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-yum -q -y install vim sudo git postgresql${PGVERSION}-server pgbouncer
+  ### Prepare user
+  mkdir ${PGHOME}/.ssh
+  echo ${SSH_PUB_KEY} >> ${PGHOME}/.ssh/authorized_keys
+  chmod 700 ${PGHOME}/.ssh
+  chmod 600 ${PGHOME}/.ssh/authorized_keys
+  chown -R postgres:postgres ${PGHOME}
+  restorecon -r -vv /var/lib/pgsql/.ssh    ### See https://ubuntuforums.org/showthread.php?t=1932058&s=00b938a71d52ba6c47b581befd8e71f6&p=12472161#post12472161
 
-### Prepare user
-mkdir ${PGHOME}/.ssh
-echo ${SSH_PUB_KEY} >> ${PGHOME}/.ssh/authorized_keys
-chmod 700 ${PGHOME}/.ssh
-chmod 600 ${PGHOME}/.ssh/authorized_keys
-chown -R postgres:postgres ${PGHOME}
-restorecon -r -vv /var/lib/pgsql/.ssh    ### See https://ubuntuforums.org/showthread.php?t=1932058&s=00b938a71d52ba6c47b581befd8e71f6&p=12472161#post12472161
-echo 'postgres    ALL=(ALL)   NOPASSWD: ALL' >> /etc/sudoers
-echo "export PATH=${PATH}" >> /etc/profile
+  ### Initialize new database
+  rm -rf ${PGDATA}/*
+  mkdir -p ${PGDATA}
+  chown postgres:postgres ${PGDATA}
+  sudo -iu postgres ${PGINSTALL}/initdb -D ${PGDATA}
+  echo 'postgres    ALL=(ALL)   NOPASSWD: ALL' >> /etc/sudoers
+  echo "export PATH=${PATH}" >> /etc/profile
+fi
 
 ### Initialize new database
 rm -rf ${PGDATA}/*
@@ -53,3 +61,9 @@ chown -R pgbouncer:pgbouncer /etc/pgbouncer
 echo '"benchuser" "md5f141e18e8635983f5f719a405cf1a49d"' > /tmp/userlist.txt
 curl -s "https://raw.githubusercontent.com/richyen/toolbox/master/vm/aws/edb/assessment/edbstore.sql" > /tmp/edbstore.sql
 ### update_data.sh located at https://gist.githubusercontent.com/richyen/3d4b1dca76a8216356e800456f36e410/raw/8a16e180abb2a5f9e7b52efd8e1dcbbcd6924222/update_data.sh
+
+echo "test environment ready"
+
+### For Docker entrypoint
+[[ ${NODOCKER} -eq 1 ]] && exit
+tail -f /dev/null
