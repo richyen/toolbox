3583ef75eab782e451136e7a1d1477210ec11784
Richard Yen <richard.yen@enterprisedb.com>
2023-05-02 11:40:48 -0700

    add custom prefix

--- utils/filename_fixes/date_prefix.sh
+++ utils/filename_fixes/date_prefix.sh
@@ -3,14 +3,15 @@
 ### Simple script to date-prefix files (usually photos or videos) based on ExifTool (https://en.wikipedia.org/wiki/ExifTool)
 
 dir=${1:-.}
-do_it=${2:-0}
+prefix=${2}
+do_it=${3:-0}
 outfile="${HOME}/Desktop/date_prefix_out.txt"
 counter=1
 for i in `ls ${dir}`
 do
   C=`exiftool ${dir}/${i} | grep "Create Date" | head -n1 | awk '{ print $4 }' | sed -e "s/:/-/g"`
   ext=`echo ${i} | cut -f2 -d'.'`
-  mv_cmd="mv ${dir}/${i} ${dir}/${C}_${counter}.${ext}"
+  mv_cmd="mv ${dir}/${i} ${dir}/${C}_${prefix}_${counter}.${ext}"
   counter=$(( counter+1 ))
   if [[ $do_it -eq 1 ]]
   then
